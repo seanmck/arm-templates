a00e1d046b687fa5839f5267009ad3ecdef1fadf
Sean McKenna <seanmck@microsoft.com>
2015-10-09 12:08:45 -0700

    Initial checkin

---
+++ onebox/SetupServiceFabricOneBox.ps1
@@ -0,0 +1,19 @@
+﻿Param(
+   [string]$Username,
+   [string]$Password
+   )
+
+# Install SDK
+
+$User = '\' + $Username
+$PWord = ConvertTo-SecureString –String $Password –AsPlainText -Force
+
+$Credential = New-Object –TypeName System.Management.Automation.PSCredential –ArgumentList $User, $PWord
+
+# Intentionally not using the user, password passed in since it doesnt work
+
+Start-Process 'C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe' -ArgumentList "/Install /XML:http://aka.ms/servicefabricinternallkg /Products:'MicrosoftAzure-ServiceFabric-InternalLKG-Develop' /AcceptEULA /Log:D:\log.txt" -NoNewWindow -Wait
+
+# Set up local cluster
+
+& 'C:\Program Files\Microsoft SDKs\Service Fabric\ClusterSetup\DevClusterSetup.ps1'
---
+++ onebox/Shortcuts.ps1
@@ -0,0 +1,35 @@
+﻿Param(
+   [string]$Username
+   )
+
+# Add link to docs to desktop
+
+$Path = 'C:\users\' + $Username + '\Desktop\'
+
+$WshShell = New-Object -comObject WScript.Shell
+$sfdocpath = $Path + 'Service Fabric Documentation.lnk'
+$Shortcut = $WshShell.CreateShortcut($sfdocpath)
+$Shortcut.TargetPath = "http://aka.ms/servicefabricdocs"
+$Shortcut.IconLocation = "C:\Windows\System32\url.dll"
+
+$Shortcut.Save()
+
+# Add link to samples to desktop
+
+$WshShell = New-Object -comObject WScript.Shell
+$sfsamplespath = $Path + 'Service Fabric Samples.lnk'
+$Shortcut = $WshShell.CreateShortcut($sfsamplespath)
+$Shortcut.TargetPath = "http://aka.ms/servicefabricsamples"
+$Shortcut.IconLocation = "C:\Windows\System32\url.dll"
+
+$Shortcut.Save()
+
+# Add shortcut to Service Fabric Explorer to desktop
+
+$WshShell = New-Object -comObject WScript.Shell
+$sfexplorerspath = $Path + 'Service Fabric Explorer.lnk'
+$Shortcut = $WshShell.CreateShortcut($sfexplorerspath)
+$Shortcut.TargetPath = 'C:\Program Files\Microsoft SDKs\Service Fabric\Tools\ServiceFabricExplorer\ServiceFabricExplorer.exe'
+$Shortcut.IconLocation = 'C:\Program Files\Microsoft SDKs\Service Fabric\Tools\ServiceFabricExplorer\ServiceFabricExplorer.exe'
+
+$Shortcut.Save()
