7fafaae165579f684e57af4216dc79d64562d857
Sean McKenna <seanmck@microsoft.com>
2016-03-03 07:49:29 -0800

    Initial commit

---
+++ samples-cluster/CreateSamplesCluster.ps1
@@ -0,0 +1,16 @@
+﻿Param(
+    [string]$ResourceGroupName,
+    [string]$SubscriptionId
+    )
+        
+$templateFile = "azuredeploy.json"
+$templateParameterFile = "azuredeploy.parameters.json"
+
+#Login-AzureRmAccount
+
+Select-AzureRmSubscription -SubscriptionId $SubscriptionId -Current
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location "eastus" -Force
+
+New-AzureRm
+
+ResourceGroupDeployment -TemplateFile $templateFile -TemplateParameterFile $templateParameterFile -ResourceGroupName $ResourceGroupName
